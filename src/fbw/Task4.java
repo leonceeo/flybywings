2179fb7e356058491c68b1704bfd782bd26382b6
Stephan Schuster <stephan.alias.pauli@gmail.com>
2013-09-18 12:55:16 +0200

    - ensure that ServerPush is loaded first - fixed PDF link in task 4

--- src/fbw/Task4.java
+++ src/fbw/Task4.java
@@ -2,7 +2,6 @@ package fbw;
 
 import org.wings.SLabel;
 import org.wings.SPanel;
-import org.wings.resource.FileResource;
 
 /**
  * This demo opens the PDF document requested in task 4 in a new browser tab.
@@ -12,8 +11,7 @@ import org.wings.resource.FileResource;
 public class Task4 extends SPanel {
 
     public Task4() {
-        final FileResource pdf = new FileResource("../doc/Choosing_a_Web_Framework.pdf");
-        pdf.getId();
-        add(new SLabel("<html><a href=\"" + pdf.getURL() + "\" target=\"_blank\">Show PDF document</a>"));
+        String pdf = "../doc/Choosing_a_Web_Framework.pdf";
+        add(new SLabel("<html><a href=\"" + pdf + "\" target=\"_blank\">Show PDF document</a>"));
     }
 }
